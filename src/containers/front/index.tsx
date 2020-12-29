6568946ade1a7f5e792b65e2fb6bb4145fb99fb5
Simon Altschuler <simon@altschuler.dk>
2020-12-29 12:45:12 +0100

    feat: front page warning

--- src/containers/front/index.tsx
+++ src/containers/front/index.tsx
@@ -5,6 +5,9 @@ export const FrontPage = () => {
     return (
         <PageLayout padded>
             <Typography.Title level={3}>Share your stuff</Typography.Title>
+            <Typography.Title type="warning" level={5}>
+                This site is not ready for any meaningful use.
+            </Typography.Title>
         </PageLayout>
     );
 };
