a2f7295f3384ff846e2f0454b2bc96762a96165f
Simon Altschuler <simon@altschuler.dk>
2022-10-06 11:09:26 +0200

    chore: update sentry config

--- next.config.js
+++ next.config.js
@@ -1,96 +1,44 @@
-﻿/* eslint-disable no-undef */
+// This file sets a custom webpack configuration to use your Next.js app
+// with Sentry.
+// https://nextjs.org/docs/api-reference/next.config.js/introduction
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
-// Use the hidden-source-map option when you don't want the source maps to be
-// publicly available on the servers, only to the error reporting
-
-// Use the SentryWebpack plugin to upload the source maps during build step
-const SentryWebpackPlugin = require('@sentry/webpack-plugin');
-const {
-    NEXT_PUBLIC_SENTRY_DSN: SENTRY_DSN,
-    SENTRY_ORG,
-    SENTRY_PROJECT,
-    SENTRY_AUTH_TOKEN,
-    NODE_ENV,
-    VERCEL_GITHUB_COMMIT_SHA,
-    VERCEL_GITLAB_COMMIT_SHA,
-    VERCEL_BITBUCKET_COMMIT_SHA,
-    NEXT_PUBLIC_NHOST_BACKEND_HOST,
-} = process.env;
-
-const COMMIT_SHA =
-    VERCEL_GITHUB_COMMIT_SHA || VERCEL_GITLAB_COMMIT_SHA || VERCEL_BITBUCKET_COMMIT_SHA;
-
-process.env.SENTRY_DSN = SENTRY_DSN;
-const basePath = '';
-
+const { withSentryConfig } = require('@sentry/nextjs');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
-})
-
-module.exports = withBundleAnalyzer({
-    images: {
-        domains: [NEXT_PUBLIC_NHOST_BACKEND_HOST],
-    },
-    env: {
-        // Make the COMMIT_SHA available to the client so that Sentry events can be
-        // marked for the release they belong to. It may be undefined if running
-        // outside of Vercel
-        NEXT_PUBLIC_COMMIT_SHA: COMMIT_SHA,
-    },
-    typescript: {
-        ignoreBuildErrors: true,
-    },
-    webpack: (config, options) => {
-        // In `pages/_app.js`, Sentry is imported from @sentry/browser. While
-        // @sentry/node will run in a Node.js environment. @sentry/node will use
-        // Node.js-only APIs to catch even more unhandled exceptions.
-        //
-        // This works well when Next.js is SSRing your page on a server with
-        // Node.js, but it is not what we want when your client-side bundle is being
-        // executed by a browser.
-        //
-        // Luckily, Next.js will call this webpack function twice, once for the
-        // server and once for the client. Read more:
-        // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
-        //
-        // So ask Webpack to replace @sentry/node imports with @sentry/browser when
-        // building the browser's bundle
-        if (!options.isServer) {
-            config.resolve.alias['@sentry/node'] = '@sentry/react';
-        }
-
-        // Define an environment variable so source code can check whether or not
-        // it's running on the server so we can correctly initialize Sentry
-        config.plugins.push(
-            new options.webpack.DefinePlugin({
-                'process.env.NEXT_IS_SERVER': JSON.stringify(options.isServer.toString()),
-            }),
-        );
-
-        // When all the Sentry configuration env variables are available/configured
-        // The Sentry webpack plugin gets pushed to the webpack plugins to build
-        // and upload the source maps to sentry.
-        // This is an alternative to manually uploading the source maps
-        // Note: This is disabled in development mode.
-        if (
-            SENTRY_DSN &&
-            SENTRY_ORG &&
-            SENTRY_PROJECT &&
-            SENTRY_AUTH_TOKEN &&
-            COMMIT_SHA &&
-            NODE_ENV === 'production'
-        ) {
-            config.plugins.push(
-                new SentryWebpackPlugin({
-                    include: '.next',
-                    ignore: ['node_modules'],
-                    stripPrefix: ['webpack://_N_E/'],
-                    urlPrefix: `~${basePath}/_next`,
-                    release: COMMIT_SHA,
-                }),
-            );
-        }
-        return config;
-    },
-    basePath,
 });
+
+const moduleExports = {
+  // Your existing module.exports
+
+  sentry: {
+    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+    // for client-side builds. (This will be the default starting in
+    // `@sentry/nextjs` version 8.0.0.) See
+    // https://webpack.js.org/configuration/devtool/ and
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+    // for more information.
+    hideSourceMaps: true,
+
+    // This option will automatically provide performance monitoring for Next.js
+    // data-fetching methods and API routes, making the manual wrapping of API
+    // routes via `withSentry` redundant.
+    autoInstrumentServerFunctions: true,
+  },
+};
+
+const sentryWebpackPluginOptions = {
+  // Additional config options for the Sentry Webpack plugin. Keep in mind that
+  // the following options are set automatically, and overriding them is not
+  // recommended:
+  //   release, url, org, project, authToken, configFile, stripPrefix,
+  //   urlPrefix, include, ignore
+
+  silent: true, // Suppresses all logs
+  // For all available options, see:
+  // https://github.com/getsentry/sentry-webpack-plugin#options.
+};
+
+// Make sure adding Sentry options is the last code to run before exporting, to
+// ensure that your source maps include changes from all other Webpack plugins
+module.exports = withBundleAnalyzer(withSentryConfig(moduleExports, sentryWebpackPluginOptions));
