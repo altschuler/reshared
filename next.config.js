082efcb6ff2fc7c4828aeea42cb2bb2159b9996f
Simon Altschuler <simon@altschuler.dk>
2022-10-06 14:19:27 +0200

    fix: add nhost domains to vercel image domains

--- next.config.js
+++ next.config.js
@@ -1,36 +1,35 @@
-// This file sets a custom webpack configuration to use your Next.js app
-// with Sentry.
-// https://nextjs.org/docs/api-reference/next.config.js/introduction
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 const { withSentryConfig } = require('@sentry/nextjs');
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
 
 const moduleExports = {
-  // Your existing module.exports
+    // Your existing module.exports
 
-  sentry: {
-    // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
-    // for client-side builds. (This will be the default starting in
-    // `@sentry/nextjs` version 8.0.0.) See
-    // https://webpack.js.org/configuration/devtool/ and
-    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
-    // for more information.
-    hideSourceMaps: true,
+    sentry: {
+        // Use `hidden-source-map` rather than `source-map` as the Webpack `devtool`
+        // for client-side builds. (This will be the default starting in
+        // `@sentry/nextjs` version 8.0.0.) See
+        // https://webpack.js.org/configuration/devtool/ and
+        // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/#use-hidden-source-map
+        // for more information.
+        hideSourceMaps: true,
 
-    // This option will automatically provide performance monitoring for Next.js
-    // data-fetching methods and API routes, making the manual wrapping of API
-    // routes via `withSentry` redundant.
-    autoInstrumentServerFunctions: true,
-  },
+        // This option will automatically provide performance monitoring for Next.js
+        // data-fetching methods and API routes, making the manual wrapping of API
+        // routes via `withSentry` redundant.
+        autoInstrumentServerFunctions: true,
+    },
+    images: {
+        domains: ['kghtoguwbjifomvtvumw.nhost.run', 'hgtsjdxovopboicvsfhk.nhost.run'],
+    },
 };
 
 const sentryWebpackPluginOptions = {
-  silent: true, // Suppresses all logs
-  dryRun: process.env.VERCEL_ENV !== "production"
+    silent: true, // Suppresses all logs
+    dryRun: process.env.VERCEL_ENV !== "production"
 };
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+});
 
 // Make sure adding Sentry options is the last code to run before exporting, to
 // ensure that your source maps include changes from all other Webpack plugins
