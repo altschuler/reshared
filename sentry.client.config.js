0c57b58a9553de6c10ff05f5f7640457f75f00b4
Simon Altschuler <simon@altschuler.dk>
2022-10-17 11:17:31 +0200

    fix(ci): properly disable sentry in dev, tests and ci

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -5,14 +5,17 @@
 import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
+const SENTRY_DISABLE = process.env.SENTRY_DISABLE || process.env.NEXT_PUBLIC_SENTRY_DISABLE;
 
-Sentry.init({
-  dsn: SENTRY_DSN || 'https://2169bd6c993847aca2e31fa821bd6909@o146636.ingest.sentry.io/5555148',
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1.0,
-  enabled: process.env.NODE_ENV === 'production' ,
-  // ...
-  // Note: if you want to override the automatic release value, do not set a
-  // `release` value here - use the environment variable `SENTRY_RELEASE`, so
-  // that it will also get attached to your source maps
-});
+if (!SENTRY_DISABLE) {
+    Sentry.init({
+        dsn: SENTRY_DSN || 'https://2169bd6c993847aca2e31fa821bd6909@o146636.ingest.sentry.io/5555148',
+        // Adjust this value in production, or use tracesSampler for greater control
+        tracesSampleRate: 1.0,
+        enabled: process.env.NODE_ENV === 'production',
+        // ...
+        // Note: if you want to override the automatic release value, do not set a
+        // `release` value here - use the environment variable `SENTRY_RELEASE`, so
+        // that it will also get attached to your source maps
+    });
+}
